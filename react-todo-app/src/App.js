fdb074f6cfca5f69d4acd1b81d5fd37c8f7eae2a
leemiran0729 <leemiran.0729@gmail.com>
2023-06-07 17:45:05 +0900

    feat: State 추가해서 삭제가능하게 구현

--- react-todo-app/src/App.js
+++ react-todo-app/src/App.js
@@ -2,6 +2,17 @@ import React, { Component } from "react";
 import "./App.css";
 
 export default class App extends Component {
+  state = {
+    todoData: [
+      {
+        id: "1",
+        title: "공부하기",
+        completed: true,
+      },
+      { id: "2", title: "청소하기", completed: false },
+    ],
+  };
+
   btnStyle = {
     color: "#fff",
     border: "none",
@@ -19,15 +30,10 @@ export default class App extends Component {
     };
   };
 
-  todoData = [
-    {
-      id: "1",
-      title: "공부하기",
-      completed: true,
-    },
-    { id: "2", title: "청소하기", completed: false },
-  ];
-
+  handleClick = (id) => {
+    let newTodoData = this.state.todoData.filter((data) => data.id !== id);
+    this.setState({ todoData: newTodoData });
+  };
   render() {
     return (
       <div className="container">
@@ -35,11 +41,16 @@ export default class App extends Component {
           <div className="title">
             <h1>할일목록</h1>
           </div>
-          {this.todoData.map((data) => (
+          {this.state.todoData.map((data) => (
             <div style={this.getStyle()} key={data.id}>
               <input type="checkbox" defaultChecked={false} />
               {data.title}
-              <button style={this.btnStyle}>X</button>
+              <button
+                style={this.btnStyle}
+                onClick={() => this.handleClick(data.id)}
+              >
+                X
+              </button>
             </div>
           ))}
         </div>

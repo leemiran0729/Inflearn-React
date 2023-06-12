1482cb755cff6c742c2397af628a6894761b7e37
leemiran0729 <leemiran.0729@gmail.com>
2023-06-12 15:03:50 +0900

    style: Form 컴포넌트 css 적용

--- react-todo-app-component/src/components/Form.js
+++ react-todo-app-component/src/components/Form.js
@@ -6,15 +6,21 @@ export default function Form({ handleSubmit, value, setValue }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="flex pt-2">
       <input
+        className="w-full px-3 py-2 mr-4 text-gray-500 border rounded shadow"
         type="text"
         name="value"
         placeholder="해야할 일을 입력하세요"
         value={value}
         onChange={handleChange}
       />
-      <input type="submit" value="입력" />
+      <input
+        type="submit"
+        className="p-2 text-blue-400 border-2 border-blue-400 rounded hover:text-white
+        hover:bg-blue-200"
+        value="입력"
+      />
     </form>
   );
 }

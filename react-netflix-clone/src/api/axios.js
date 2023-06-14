d314795eb9b021e9c53b124b56481e571f4a3730
leemiran0729 <leemiran.0729@gmail.com>
2023-06-14 15:08:35 +0900

    feat: 비디오 배너 생성

--- react-netflix-clone/src/api/axios.js
+++ react-netflix-clone/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "",
+    api_key: "50bda7b34f8d8f1969dfd455cf267ad3",
     language: "ko-KR",
   },
 });

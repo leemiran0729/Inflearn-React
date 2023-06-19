b1ffa7c37d73062f863a7be4d63d8b5bdff6fe53
leemiran0729 <leemiran.0729@gmail.com>
2023-06-20 00:38:21 +0900

    feat: 화살표 스크롤 이동 기능 구현

--- react-netflix-clone/src/components/Row.js
+++ react-netflix-clone/src/components/Row.js
@@ -19,7 +19,14 @@ const Row = ({ isLargeRow, title, id, fetchUrl }) => {
       <h2>{title}</h2>
       <div className="slider">
         <div className="slider__arrow-left">
-          <span className="arrow">{"<"}</span>
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}
+          >
+            {"<"}
+          </span>
         </div>
         <div id={id} className="row__posters">
           {movies &&
@@ -35,7 +42,14 @@ const Row = ({ isLargeRow, title, id, fetchUrl }) => {
             ))}
         </div>
         <div className="slider__arrow-right">
-          <span className="arrow">{">"}</span>
+          <span
+            className="arrow"
+            onClick={() => {
+              document.getElementById(id).scrollLeft += window.innerWidth - 80;
+            }}
+          >
+            {">"}
+          </span>
         </div>
       </div>
     </section>

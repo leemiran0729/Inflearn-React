3f55499d487682aff5a1486ff507394fda6c8023
leemiran0729 <leemiran.0729@gmail.com>
2023-06-21 17:06:20 +0900

    feat: useDebounce hook 적용

--- react-netflix-clone/src/pages/SearchPage/index.js
+++ react-netflix-clone/src/pages/SearchPage/index.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import axios from "../../api/axios";
 import "./SearchPage.css";
+import { useDebounce } from "../../hooks/useDebounce";
+
 const SearchPage = () => {
   const [searchResults, setSearchResults] = useState([]);
 
@@ -11,12 +13,13 @@ const SearchPage = () => {
 
   let query = useQuery();
   const searchTerm = query.get("q");
+  const debouncedSearchTerm = useDebounce(searchTerm, 500);
 
   useEffect(() => {
-    if (searchTerm) {
-      fetchSearchMovie(searchTerm);
+    if (debouncedSearchTerm) {
+      fetchSearchMovie(debouncedSearchTerm);
     }
-  }, [searchTerm]);
+  }, [debouncedSearchTerm]);
 
   const fetchSearchMovie = async (searchTerm) => {
     try {
@@ -53,7 +56,9 @@ const SearchPage = () => {
     ) : (
       <section className="no-results">
         <div className="no-results__text">
-          <p>찾고자하는 검색어"{searchTerm}"에 맞는 영화가 없습니다.</p>
+          <p>
+            찾고자하는 검색어"{debouncedSearchTerm}"에 맞는 영화가 없습니다.
+          </p>
         </div>
       </section>
     );

6502fb632206d0202ee8f64218de2916136b1dda
leemiran0729 <leemiran.0729@gmail.com>
2023-06-27 02:38:33 +0900

    style: home, 포스트 상세 스타일 적용

--- nextjs-app/pages/index.tsx
+++ nextjs-app/pages/index.tsx
@@ -14,7 +14,7 @@ const Home = ({
   }[];
 }) => {
   return (
-    <div>
+    <div className={homeStyles.container}>
       <Head>
         <title>Miran Lee</title>
       </Head>
--- nextjs-app/posts/[id].tsx
+++ nextjs-app/posts/[id].tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { getAllPostIds, getPostData } from "../lib/post";
 import Head from "next/head";
+import postStyles from "../styles/Post.module.css";
 
 const Post = ({
   postData,
@@ -13,7 +14,7 @@ const Post = ({
   };
 }) => {
   return (
-    <div>
+    <div className={postStyles.container}>
       <Head>
         <title>{postData.title}</title>
       </Head>

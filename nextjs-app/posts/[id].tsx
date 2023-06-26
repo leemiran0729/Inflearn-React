d8bb3e54ef10e22a8ceb026fb1f64c814baf1ded
leemiran0729 <leemiran.0729@gmail.com>
2023-06-27 01:42:28 +0900

    feat: getAllPostIds, getStaticPaths 구현

--- nextjs-app/posts/[id].tsx
+++ nextjs-app/posts/[id].tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { GetStaticPaths } from "next";
+import { getAllPostIds } from "../lib/post";
 
 const Post = () => {
   return <div>Post</div>;
 };
 
 export default Post;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  // [{params: {id: 'pre-rendering'}, {}...}]
+  return {
+    paths,
+    fallback: false,
+  };
+};
